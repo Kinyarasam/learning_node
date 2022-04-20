87cc5c99309e9cf5f73cbad546e56b31b1bedc4d
kinyarasam <samkinyara2000@gmail.com>
2022-04-20 21:33:25 +0300

    Set

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+
+// Set static Folder
+app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = process.env.PORT || 5000
 
