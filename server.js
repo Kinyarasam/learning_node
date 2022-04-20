262512dca1df0d388eb1f206168a04cf942bcd64
kinyarasam <samkinyara2000@gmail.com>
2022-04-20 21:51:58 +0300

    node requests and middlewares

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const members = require('./model/Members')
+
+const logger = (req, res, next) => {
+	console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`)
+	next()
+}
+
+// Init middleware
+app.use(logger) 
+
+//Gets All Members
+app.get('/api/members', (req, res) => {
+	res.json(members)
+})
 
 // Set static Folder
 app.use(express.static(path.join(__dirname, 'public')))
